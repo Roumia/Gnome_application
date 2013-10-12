bd89739895e29a909623c0c5a2e590ad70f490df
Roumia <roumia@gmail.com>
2013-10-12 12:34:16 +0200

    Create Buttom.c

---
+++ Buttom.c
@@ -0,0 +1,50 @@
+#include <gtk/gtk.h>
+
+static void
+print_hello (GtkWidget *widget,
+     gpointer   data)
+{
+  g_print ("Desktop application :* \n");
+}
+
+
+
+int
+main (int   argc,char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *grid;
+  GtkWidget *button;
+
+
+  gtk_init (&argc, &argv);  /* Called in all GTK applications */
+
+  /* create a  window */
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (window), "MSWL windows");
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 100);
+
+  
+  grid = gtk_grid_new (); /*  container for buttons */
+
+  gtk_container_add (GTK_CONTAINER (window), grid);
+
+  button = gtk_button_new_with_label ("Try me ");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 0, 1, 1);
+
+	
+
+  button = gtk_button_new_with_label ("Quit!");
+  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 1, 1, 1);
+
+
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+
+  return 0;
+}
