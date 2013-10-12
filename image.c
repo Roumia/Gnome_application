2c16d4c4ff91835734988fdfd06d5a5eda430fd9
Roumia <roumia@gmail.com>
2013-10-12 12:36:21 +0200

    Update image.c

--- image.c
+++ image.c
@@ -11,11 +11,9 @@ int main( int argc, char *argv[])
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 230, 150);
   gtk_window_set_title(GTK_WINDOW(window), "Urjc");
-  gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
-  gtk_container_set_border_width(GTK_CONTAINER(window), 100);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
 
   image = gtk_image_new_from_file("logoURJC.png");
