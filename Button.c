9d7333005b0a4358a173c63406d42fe8133c13e6
Roumia <roumia@gmail.com>
2013-10-15 03:22:02 +0200

    Update Button.c

--- Button.c
+++ Button.c
@@ -4,10 +4,22 @@ static void
 print_hello (GtkWidget *widget,
      gpointer   data)
 {
-  g_print ("Desktop application :* \n");
+  g_print ("Desktop application -----   :* \n");
 }
 
+ static void
+open_image (GtkButton *button, gpointer user_data)
+{
+	GtkWidget *image ;
+    GtkWidget *window;
 
+    image = gtk_image_new_from_file ("logoURJC.png");
+
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_container_add (GTK_CONTAINER (window), image);
+
+    gtk_widget_show_all (window);
+}
 
 int
 main (int   argc,char *argv[])
@@ -35,16 +47,19 @@ main (int   argc,char *argv[])
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
   gtk_grid_attach (GTK_GRID (grid), button, 1, 0, 1, 1);
 
-	
+ button = gtk_button_new_with_label ("URJC   logo");
+ g_signal_connect (button, "clicked", G_CALLBACK (open_image), NULL);
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 1, 1, 1);
 
   button = gtk_button_new_with_label ("Quit!");
   g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
-  gtk_grid_attach (GTK_GRID (grid), button, 1, 1, 1, 1);
 
 
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 2,1,1);
   gtk_widget_show_all (window);
 
   gtk_main ();
 
   return 0;
 }
+
